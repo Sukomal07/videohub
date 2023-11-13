4f3ea7b7e782ac0e78fa11fa12e2b7c4ba88853f
Sukomal07 <hypersukomal@gmail.com>
2023-11-13 10:52:47 +0530

    config cloudinary

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,22 @@
 import app from './app.js'
-import {connectDb} from './db/db.js'
+import { connectDb } from './db/db.js'
 import dotenv from 'dotenv'
+import { v2 as cloudinary } from 'cloudinary';
+
 
 dotenv.config()
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-app.listen(process.env.PORT, () =>{
-    connectDb()
-    console.log(`server is working on http://localhost:${process.env.PORT}`);
-})
+connectDb()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on http://localhost:${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed:", err);
+    });
