813985dedc2489778ab44d30427edb3f50db0e4a
Sukomal07 <hypersukomal@gmail.com>
2023-11-20 22:18:23 +0530

    create user controller and routes

--- server/src/app.js
+++ server/src/app.js
@@ -6,11 +6,19 @@ import cookieParser from 'cookie-parser'
 const app = express();
 
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true
+    origin: process.env.FRONTEND_URL,
+    credentials: true
 }))
-app.use(express.json({limit:"20kb"}))
-app.use(express.urlencoded({extended:true, limit:"20kb"}))
+app.use(express.json({ limit: "20kb" }))
+app.use(express.urlencoded({ extended: true, limit: "20kb" }))
 app.use(cookieParser())
+
+// import routes
+import userRoutes from './routes/user.routes.js'
+
+
+//routes config
+
+app.use("/api/v1/user", userRoutes)
 
 export default app
