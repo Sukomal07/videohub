4d1c80b6457ac3627881aab270296d92cfaf1494
Sukomal07 <hypersukomal@gmail.com>
2023-12-17 19:44:15 +0530

    change minor issue

--- server/src/middlewares/error.middleware.js
+++ server/src/middlewares/error.middleware.js
@@ -1,8 +1,8 @@
 const errorMiddleware = (err, req, res, next) => {
     console.error(err.stack);
 
-    res.status(err.statusCode || 500).json({
-        status: err.statusCode || 500,
+    res.status(err.status || 500).json({
+        status: err.status || 500,
         message: err.message || "Internal Server Error",
         errors: err.errors || [],
     });
