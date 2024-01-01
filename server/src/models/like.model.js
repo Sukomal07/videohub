5cfccdd28b139cd9a08689d2dfd7366ca7f75219
Sukomal07 <hypersukomal@gmail.com>
2024-01-01 21:08:19 +0530

    added like model

---
+++ server/src/models/like.model.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const likeSchema = new Schema({
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    },
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: 'Video'
+    },
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    tweet: {
+        type: Schema.Types.ObjectId,
+        ref: 'Tweet'
+    }
+})
+
+const Like = model('Like', likeSchema)
+
+export default Like
