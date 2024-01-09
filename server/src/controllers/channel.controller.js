1fe3a004ef8d953deb727fb5970cd4b8c53462a2
Sukomal07 <hypersukomal@gmail.com>
2024-01-09 21:24:27 +0530

    added all channel apis

--- server/src/controllers/channel.controller.js
+++ server/src/controllers/channel.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import Video from "../models/video.model.js"
 import { ApiError } from "../utils/apiError.js"
 import { ApiResponse } from "../utils/apiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
@@ -74,5 +75,199 @@ export const getChannelProfile = asyncHandler(async (req, res) => {
 
     res.status(200).json(
         new ApiResponse(200, channel[0], "Channel fetched successfully")
+    )
+})
+
+export const getChannelVideos = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    if (!username) {
+        throw new ApiError(400, 'username is required')
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                userName: username
+            }
+        },
+        {
+            $lookup: {
+                from: 'videos',
+                localField: '_id',
+                foreignField: 'owner',
+                as: 'videos',
+                pipeline: [
+                    {
+                        $project: {
+                            title: 1,
+                            thumbnail: 1,
+                            videoFile: 1,
+                            views: 1,
+                            createdAt: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                videos: "$videos"
+            }
+        }
+    ])
+
+    if (!channel?.length) {
+        throw new ApiError(404, 'videos not found')
+    }
+
+    res.status(200).json(
+        new ApiResponse(200, channel[0].videos, 'videos fetched successfully')
+    )
+})
+
+export const getChannelPlaylist = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    if (!username) {
+        throw new ApiError(400, 'username is required')
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                userName: username
+            }
+        },
+        {
+            $lookup: {
+                from: 'playlists',
+                localField: '_id',
+                foreignField: 'owner',
+                as: 'playlists',
+                pipeline: [
+                    {
+                        $project: {
+                            name: 1,
+                            description: 1,
+                            videos: 1,
+                            createdAt: 1
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    if (!channel?.length) {
+        throw new ApiError(400, 'Playlist not found')
+    }
+
+    res.status(200).json(
+        new ApiResponse(200, channel[0].playlists, 'Playlist fetched successfully')
+    )
+})
+
+export const getChannelTweets = asyncHandler(async (req, res) => {
+    const { username } = req.params
+
+    if (!username) {
+        throw new ApiError(400, 'username is required')
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                userName: username
+            }
+        },
+        {
+            $lookup: {
+                from: 'tweets',
+                localField: '_id',
+                foreignField: 'owner',
+                as: 'tweets',
+                pipeline: [
+                    {
+                        $project: {
+                            content: 1,
+                            createdAt: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                tweets: "$tweets"
+            }
+        }
+    ])
+
+    if (!channel?.length) {
+        throw new ApiError(404, 'Tweets not found')
+    }
+
+    res.status(200).json(
+        new ApiResponse(200, channel[0].tweets, 'Tweets fetched successfully')
+    )
+})
+
+export const getAllFollowings = asyncHandler(async (req, res) => {
+    const { username } = req.params
+
+    if (!username) {
+        throw new ApiError(400, 'username is required')
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                userName: username
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "followings",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "channel",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        userName: 1,
+                                        avatar: 1,
+                                        createdAt: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    if (!channel.length) {
+        throw new ApiError(404, 'followings not found')
+    }
+
+    res.status(200).json(
+        new ApiResponse(200, channel[0].followings, 'followings fetched successfully')
     )
 })
--- server/src/routes/channel.routes.js
+++ server/src/routes/channel.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getChannelProfile } from '../controllers/channel.controller.js'
+import { getAllFollowings, getChannelPlaylist, getChannelProfile, getChannelTweets, getChannelVideos } from '../controllers/channel.controller.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js'
 import { upload } from '../middlewares/multer.middleware.js'
 import { deleteVideo, getAllVideo, getVideoById, togglePublishStatus, updateVideo, uploadNewVideo } from '../controllers/video.controller.js'
@@ -8,10 +8,13 @@ const router = express.Router()
 
 router.get('/details/:username', getChannelProfile)
 router.post('/new/upload', verifyJWT, upload.fields([{ name: "videoFile", maxCount: 1 }, { name: "thumbnail", maxCount: 1 }]), uploadNewVideo)
-router.get('/videos', getAllVideo)
+router.get('/:username', getChannelVideos)
 router.get('/videos/:videoId', getVideoById)
 router.patch('/videos/:videoId', verifyJWT, upload.single("thumbnail"), updateVideo)
 router.delete('/videos/delete/:videoId', verifyJWT, deleteVideo)
 router.patch('/videos/:videoId/toggle', verifyJWT, togglePublishStatus)
 
+router.get('/:username/playlists', getChannelPlaylist)
+router.get('/:username/tweets', getChannelTweets)
+router.get('/:username/followings', getAllFollowings)
 export default router
